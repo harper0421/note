8cab0ce8ba87b36d18c358fc03b985dfd5f17219
hepeng <1942854896@qq.com>
2024-11-06 10:40:51 +0800

    dist

--- .vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme';
-import Foo from './components/Foo.vue';
-
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('foo', Foo);
-  },
-};
